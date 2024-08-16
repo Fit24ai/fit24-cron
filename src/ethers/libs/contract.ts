59e70e8398da39d14ad1b3c099b77ac1365a90e1
robinsarawat0 <robinsaraswat9@gmail.com>
2024-08-16 15:53:31 +0530

    fixes

--- src/ethers/libs/contract.ts
+++ src/ethers/libs/contract.ts
@@ -1,2 +1,2 @@
 export const IcoContract = '0x19ec94D98178E11187D118e490E464A424a4fDD8'
-export const StakingContract = "0x3A0a9164fAC293f66aD2815384bCb126F4cB5cDF"
+export const StakingContract = "0xf49b1767444BAFb499E1e4c5E0603CABF89089D8"
